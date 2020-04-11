cc3b02d9b4a1db3fad57b171e9440d992e6c073e
armaank <kohli@cooper.edu>
2020-04-11 16:57:05 -0400

    added ckpts

--- train.py
+++ train.py
@@ -16,32 +16,77 @@ class Trainer(object):
 
     def __init__(self, args):
 
+        # argument gathering
+        self.rand_seed = args.rand_seed
+        self.use_cuda = args.cuda and torch.cuda.is_available()
+        self.n_iter = args.n_iter
+        self.batch_size = args.batch_size
+        self.optim = args.optim
+        self.lr = args.lr
+        self.beta1 = args.beta1
+        self.beta2 = args.beta2
+        self.data_dir = args.data_dir
+        self.output_dir = args.output
+        self.embed = args.embed
+        self.embed_dim = args.embed_dim
+        self.hidden_dim = args.hidden_dim
+        self.ckpt_dir = args.ckpt_dir
+        self.ckpt_name = args.ckpt
+
+        self.global_iter = 0
+
         pass
 
     def train(self):
         """
         trains a network
-
-        todo: train/validation split
         """
 
         return 0
-
-    def load_ckpt(self):
+    
+    def load_ckpt(self, filename):
         """
-        loads a checkpoint
-
+        load checkpoint
         """
 
+        file_path = os.path.join(self.ckpt_dir, filename)
+
+        if os.path.isfile(file_path):
+
+            checkpoint = torch.load(file_path)
+
+            self.global_iter = checkpoint["iter"]
+            self.net.load_state_dict(checkpoint["model_states"]["net"])
+            self.optim.load_state_dict(checkpoint["optim_states"]["optim"])
+
+            print("=> loaded ckpt '{} (iter {})'".format(file_path, self.global_iter))
+
+        else:
+            print("=> no ckpt at '{}'".format(file_path))
+        
         pass
 
-    def save_ckpt(self):
-        """ 
-        saves a model checkpoint
+   def save_ckpt(self, filename):
+        """
+        save a model checkpoint
+        saves best checkpoint always
         """
 
-        pass
+        model_states = {"net": self.net.state_dict()}
+        optim_states = {"optim": self.optim.state_dict()}
 
+        states = {
+            "iter": self.global_iter,
+            "model_states": model_states,
+            "optim_states": optim_states,
+        }
+
+        file_path = os.path.join(self.ckpt_dir, filename)
+
+        with open(file_path, mode="wb+") as f:
+            torch.save(states, f)
+
+        pass
 
 def main(args):
     """
@@ -65,38 +110,25 @@ if __name__ == "__main__":
     parser.add_argument("--cuda", default=True, type=bool, help="enable cuda")
     parser.add_argument("--n_iter", default=1e6, type=float, help="num of grad. steps")
     parser.add_argument("--batch_size", default=32, type=int, help="batch size")
-    parser.add_argument("--lr", default=1e-4, type=float, help="ADAM learning rate")
+    parser.add_argument("--optim", default="ADAM", type=int, help="optimizer")
+    parser.add_argument("--lr", default=1e-4, type=float, help="learning rate")
     parser.add_argument("--beta1", default=0.9, type=float, help="ADAM beta1")
     parser.add_argument("--beta2", default=0.999, type=float, help="ADAM beta2")
     parser.add_argument("--data_dir", default="data", type=str, help="data directory")
     parser.add_argument("--output_dir", default="outputs", type=str, help="output dir")
-    parser.add_argument("--n_workers", default=2, type=int, help="dataloader n_workers")
     parser.add_argument("--embed", default="typ", type=int, help="type of embedding")
     parser.add_argument("--embed_dim", default="128", type=int, help="embedding dim.")
     parser.add_argument("--hidden_dim", default="32", type=int, help="lstm hidden dim.")
-
+    parser.add_argument("--load_cktp", default=FALSE, type=bool, help="load ckpt or nah")
+    parser.add_argument("--ckpt_name", default="last", type=str, help="checkpoint name")
     parser.add_argument(
         "--ckpt_dir",
-        default="outputs/saved_models/ckpts/experiment",
+        default="outputs/saved_models/ckpts/",
         type=str,
-        help="checkpoint dir",
+        help="where to save ckpts",
     )
-    # modify loading of ckpts
-    parser.add_argument(
-        "--save_step",
-        default=100,
-        type=int,
-        help="num of grad steps before checkpoints are saved",
-    )
+    # maybe add lr scheduler? 
 
-    parser.add_argument(
-        "--ckpt_name",
-        default="last",
-        type=str,
-        help="load previous checkpoint. insert checkpoint filename",
-    )
-
-    # add lr decay, sgd lr
 
     args = parser.parse_args()
 
