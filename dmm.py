c50b00ecfe901c2115f2edd83cd71311c3fa3e95
armaank <kohli@cooper.edu>
2020-04-28 10:25:43 -0400

    finished gate, combiner, emitter

--- dmm.py
+++ dmm.py
@@ -1,2 +1,150 @@
-"""model
+"""dmm
 """
+import argparse
+import os
+
+import numpy as np
+import torch
+import torchtext
+import pyro
+
+import torch.nn as nn
+import pyro.distributions as dist
+import pyro.poutine as poutine
+
+from torch.autograd import Variable
+from pyro.distributions import TransformedDistribution
+
+
+class Emitter(nn.Module):
+    """
+    parameterizes the categorical observation likelihood p(x_t|z_t)
+    """
+
+    def __init__(self, input_dim, z_dim, emission_dim):
+        super().__init__()
+        """
+        initilize the fcns used in the network
+        """
+        self.lin_z_to_hidden = nn.Linear(z_dim, emission_dim)
+        self.lin_hidden_to_hidden = nn.Linear(emission_dim, emission_dim)
+        self.lin_hidden_to_input = nn.Linear(emission_dim, input_dim)
+        self.relu = nn.ReLU()
+
+        pass
+
+    def forward(self, z_t):
+        """
+        given z_t, compute the probabilities that parameterizes the categorical distribution p(x_t|z_t)
+        """
+        h1 = self.relu(self.lin_z_to_hidden(z_t))
+        h2 = self.relu(self.lin_hidden_to_hidden(h1))
+        probs = torch.sigmoid(
+            self.lin_hidden_to_input(h2)
+        )  # might need to change to argmax, max?, softmax?
+
+        return probs
+
+
+class GatedTransition(nn.Module):
+    """
+    parameterizes the gaussian latent transition probability p(z_t | z_{t-1})
+    """
+
+    def __init__(self, z_dim, transition_dim):
+        super().__init__()
+        """
+        initilize the fcns used in the network
+        """
+        self.lin_gate_z_to_hidden = nn.Linear(z_dim, transition_dim)
+        self.lin_gate_hidden_to_z = nn.Linear(transition_dim, z_dim)
+        self.lin_proposed_mean_z_to_hidden = nn.Linear(z_dim, transition_dim)
+        self.lin_proposed_mean_hidden_to_z = nn.Linear(transition_dim, z_dim)
+        self.lin_sig = nn.Linear(z_dim, z_dim)
+        self.lin_z_to_loc = nn.Linear(z_dim, z_dim)
+
+        self.lin_z_to_loc.weight.data = torch.eye(z_dim)
+        self.lin_z_to_loc.bias.data = torch.zeros(z_dim)
+
+        self.relu = nn.ReLU()
+        self.softplus = nn.Softplus()
+
+        pass
+
+    def forward(self, z_t_1):
+        """
+        Given the latent z_{t-1} we return the mean and scale vectors that parameterize the
+        (diagonal) gaussian distribution p(z_t | z_{t-1})`
+        """
+        # compute the gating function
+        _gate = self.relu(self.lin_gate_z_to_hidden(z_t_1))
+        gate = torch.sigmoid(self.lin_gate_hidden_to_z(_gate))
+        # compute the 'proposed mean'
+        _proposed_mean = self.relu(self.lin_proposed_mean_z_to_hidden(z_t_1))
+        proposed_mean = self.lin_proposed_mean_hidden_to_z(_proposed_mean)
+        # assemble the actual mean used to sample z_t, which mixes a linear transformation
+        # of z_{t-1} with the proposed mean modulated by the gating function
+        loc = (1 - gate) * self.lin_z_to_loc(z_t_1) + gate * proposed_mean
+        # compute the scale used to sample z_t, using the proposed mean from
+        # above as input the softplus ensures that scale is positive
+        scale = self.softplus(self.lin_sig(self.relu(proposed_mean)))
+        # return loc, scale which can be fed into Normal
+        return loc, scale
+
+
+class Combiner(nn.Module):
+    """
+    parameterizes q(z_t | z_{t-1}, x_{t:T}), which is the basic building block
+    of the guide (i.e. the variational distribution). The dependence on x_{t:T} is
+    through the hidden state of the RNN
+    """
+
+    def __init__(self, z_dim, rnn_dim):
+        super().__init__()
+        """
+        initilize the fcns used in the network
+        """
+        self.lin_z_to_hidden = nn.Linear(z_dim, rnn_dim)
+        self.lin_hidden_to_loc = nn.Linear(rnn_dim, z_dim)
+        self.lin_hidden_to_scale = nn.Linear(rnn_dim, z_dim)
+        self.tanh = nn.Tanh()
+        self.softplus = nn.Softplus()
+
+        pass
+
+    def forward(self, z_t_1, h_rnn):
+        """
+        Given the latent z_{t-1} at at a particular time as well as the hidden
+        state of the RNN h(x_{t:T}) we return the mean and scale vectors that
+        parameterize the (diagonal) gaussian distribution q(z_t | z_{t-1}, x_{t:T})
+        """
+        # combine the rnn hidden state with a transformed version of z_t_1
+        h_combined = 0.5 * (self.tanh(self.lin_z_to_hidden(z_t_1)) + h_rnn)
+        # use the combined hidden state to compute the mean used to sample z_t
+        loc = self.lin_hidden_to_loc(h_combined)
+        # use the combined hidden state to compute the scale used to sample z_t
+        scale = self.softplus(self.lin_hidden_to_scale(h_combined))
+        # return loc, scale which can be fed into Normal
+        return loc, scale
+
+
+class DMM(nn.Module):
+    """
+    module for the model and the guide (variational distribution) for the DMM
+    """
+
+    def __init__():
+        super().__init__()
+        """
+        text
+        """
+
+        pass
+
+    def model():
+
+        pass
+
+    def guide():
+
+        pass
--- utils.py
+++ utils.py
@@ -1,2 +1,18 @@
 """utils
+todo: add comments
 """
+import logging
+
+
+def get_logger(log_file):
+    logging.basicConfig(
+        level=logging.DEBUG, format="%(message)s", filename=log_file, filemode="w"
+    )
+    console = logging.StreamHandler()
+    console.setLevel(logging.INFO)
+    logging.getLogger("").addHandler(console)
+
+    def log(s):
+        logging.info(s)
+
+    return log
